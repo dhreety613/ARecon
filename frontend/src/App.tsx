0c6a7049f8ca74d655e0ab7bfd956067b79369af
dhreety613 <dhreetysatapathy@gmail.com>
2026-04-18 18:58:09 +0530

    page to page flow established plus home page addition after auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Auth from "./pages/Auth";
-import Surveillance from "./pages/Surveillance";
+import Home from "./pages/Home";
 import Analysis from "./pages/Analysis";
 import Missions from "./pages/Missions";
 import LiveMission from "./pages/LiveMission";
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/surveillance" element={<Surveillance />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/analysis" element={<Analysis />} />
         <Route path="/missions" element={<Missions />} />
         <Route path="/live" element={<LiveMission />} />
